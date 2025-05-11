5cc314969c10ba3b8f353b5d3afb4510421740c9
yashrajOjha <yashrajojha28@gmail.com>
2025-05-11 21:25:16 +0530

    minor fix

--- model.py
+++ model.py
@@ -172,7 +172,9 @@ class DecoderBlock(nn.Module):
         self.self_attention_block = self_attention_block
         self.cross_attention_block = cross_attention_block
         self.feed_forward_block = feed_forward_block
-        self.residual_connections = nn.Module([ResidualConnection(dropout) for _ in range(3)])
+        self.residual_connections = nn.ModuleList([
+            ResidualConnection(dropout) for _ in range(3)
+        ])
 
     def forward(self, x, encoder_output, src_mask, target_mask):
         x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, target_mask))
